13bdc1fb456733a309176efa6d39a101296cb118
Jolene Okamoto <51222821+joleneokamoto@users.noreply.github.com>
2022-04-01 11:16:38 -0400

    Changing bulletinpost id to nanoid (#28)

--- services/townService/src/models/posts/dao.ts
+++ services/townService/src/models/posts/dao.ts
@@ -1,20 +1,18 @@
-import { Schema } from 'mongoose';
 import PostModel from './model';
 import ServerBulletinPost from '../../types/BulletinPost';
-import { PostCreateRequest } from '../../CoveyTypes';
 
-export async function createPost(post: PostCreateRequest) : Promise<ServerBulletinPost> {
+export async function createPost(post: ServerBulletinPost) : Promise<ServerBulletinPost> {
   const response: ServerBulletinPost = await PostModel.create(post);
   return response;
 } 
 
 // TODO change return type (mongo returns a boolean and number of objects deleted - would need to make a type)?
-export async function deletePost(id: Schema.Types.ObjectId) : Promise<void> {
-  await PostModel.deleteOne({ _id:  id });
+export async function deletePost(id: string) : Promise<void> {
+  await PostModel.deleteOne({ id });
 } 
 
-export async function findPostById(id: Schema.Types.ObjectId) : Promise<ServerBulletinPost | null> {
-  const response: ServerBulletinPost | null = await PostModel.findById(id);
+export async function findPostById(id: string) : Promise<ServerBulletinPost | null> {
+  const response: ServerBulletinPost | null = await PostModel.findOne({ id });
   return response;
 } 
 
