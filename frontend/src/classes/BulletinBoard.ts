d1086357f341197100938231995aaf9d6d4325e6
Justin Bian <justinbian10@gmail.com>
2022-04-24 19:35:50 -0400

    Add frontend documentation

--- frontend/src/classes/BulletinBoard.ts
+++ frontend/src/classes/BulletinBoard.ts
@@ -1,6 +1,8 @@
 import BulletinPost from './BulletinPost';
 
-
+/**
+ * Frontend class representation of a bulletin board. Contains a list of bulletin posts that can be accessed.
+ */
 export default class BulletinBoard {
   private _posts: BulletinPost[] = [];
 
@@ -8,14 +10,25 @@ export default class BulletinBoard {
     return this._posts;
   }
 
+  /**
+   * Checks if bulletin board is empty
+   * @returns boolean representing if there are no posts in the bulletin board
+   */
   isEmpty(): boolean {
     return this._posts.length === 0;
   }
 
+  /**
+   * Function to add a post to the current bulletin board
+   * @param post bulletin post to add to the bulletin board
+   */
   addPost(post: BulletinPost) {
     this._posts.push(post);
   }
 
+  /**
+   * Function that sorts the bulletin posts in the board by time
+   */
   sortPostsByTime() {
     this._posts.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
   }
--- frontend/src/components/BulletinBoard/BulletinPost.tsx
+++ frontend/src/components/BulletinBoard/BulletinPost.tsx
@@ -3,25 +3,58 @@ import React from 'react';
 import { BulletinPostSchema } from '../../classes/BulletinPost';
 
 type BulletinPostProps = {
-    post: BulletinPostSchema;
-}
-export default function BulletinPost({post} : BulletinPostProps) : JSX.Element {
-    const date = new Date(post.createdAt);
-    return (<Box rounded="20px" overflow="hidden" borderWidth='1px' boxShadow='md' bg='gray.200' p={8} mt={5} mb={5}>
-        <Heading as='h2' size='md' >{post.title}</Heading>
-        <Text as='span' color='gray.500'
-            fontWeight='semibold'
-            letterSpacing='wide'
-            fontSize='sm'
-            textTransform='uppercase'
-            isTruncated>{post.author}</Text>
-        <Text as='span' color='gray.500'
-            fontWeight='semibold'
-            letterSpacing='wide'
-            fontSize='sm'
-            textTransform='uppercase'
-            style={{float: 'right'}}
-            isTruncated>{date.toLocaleString([], {month: 'short', day: 'numeric', hour: 'numeric', minute: '2-digit'})}</Text>
-        <Text fontSize='sm' mt={5}>{post.text}</Text>
-    </Box>)
+  post: BulletinPostSchema;
+};
+
+/**
+ * React component that represents a bulletin post in a bulletin board.
+ * @param param0 prop containing info about the bulletin board that should be is rendered by this component
+ * @returns
+ */
+export default function BulletinPost({ post }: BulletinPostProps): JSX.Element {
+  const date = new Date(post.createdAt);
+  return (
+    <Box
+      rounded='20px'
+      overflow='hidden'
+      borderWidth='1px'
+      boxShadow='md'
+      bg='gray.200'
+      p={8}
+      mt={5}
+      mb={5}>
+      <Heading as='h2' size='md'>
+        {post.title}
+      </Heading>
+      <Text
+        as='span'
+        color='gray.500'
+        fontWeight='semibold'
+        letterSpacing='wide'
+        fontSize='sm'
+        textTransform='uppercase'
+        isTruncated>
+        {post.author}
+      </Text>
+      <Text
+        as='span'
+        color='gray.500'
+        fontWeight='semibold'
+        letterSpacing='wide'
+        fontSize='sm'
+        textTransform='uppercase'
+        style={{ float: 'right' }}
+        isTruncated>
+        {date.toLocaleString([], {
+          month: 'short',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: '2-digit',
+        })}
+      </Text>
+      <Text fontSize='sm' mt={5}>
+        {post.text}
+      </Text>
+    </Box>
+  );
 }
