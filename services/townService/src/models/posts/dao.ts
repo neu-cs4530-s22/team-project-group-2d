f9e3d8ad00de7eddb9bfa949ab074ee4f80e0e37
Jolene Okamoto <51222821+joleneokamoto@users.noreply.github.com>
2022-04-25 00:21:31 -0400

    Revert "Remove Mongo files from db (#50)"
    This reverts commit cd5e0a2806c6834a959047974cb736321f054d80.

---
+++ services/townService/src/models/posts/dao.ts
@@ -0,0 +1,36 @@
+import { BulletinPostSchema } from '../../types/BulletinPost';
+import PostModel from './model';
+
+export interface DeletedPostsResponse {
+  deletedCount: number;
+}
+
+export async function createPost(post: BulletinPostSchema): Promise<BulletinPostSchema> {
+  const response: BulletinPostSchema = await PostModel.create(post);
+  return response;
+}
+
+// TODO change return type (mongo returns a boolean and number of objects deleted - would need to make a type)?
+export async function deletePost(id: string): Promise<void> {
+  await PostModel.deleteOne({ id });
+}
+
+export async function findPostById(id: string): Promise<BulletinPostSchema | null> {
+  const response: BulletinPostSchema | null = await PostModel.findOne({ id });
+  return response;
+}
+
+export async function findAllPosts(): Promise<BulletinPostSchema[]> {
+  const response: BulletinPostSchema[] = await PostModel.find();
+  return response;
+}
+
+export async function findAllPostsInTown(townID: string): Promise<BulletinPostSchema[]> {
+  const response: BulletinPostSchema[] = await PostModel.find({ coveyTownID: townID });
+  return response;
+}
+
+export async function deletePostsBeforeDate(date: Date): Promise<DeletedPostsResponse> {
+  const response: DeletedPostsResponse = await PostModel.deleteMany({ createdAt: { $lt: date } });
+  return response;
+}
